3cecc2701b0bf0cb79553e7c1102e3b39f9275e2
Ragheed Ma'rouf Huneineh <ragheedmhuneineh@outlook.com>
2017-05-02 11:46:59 +0200

    Add HabitDbHelper file

---
+++ app/src/main/java/com/example/android/habittracker/HabitDbHelper.java
@@ -0,0 +1,10 @@
+package com.example.android.habittracker;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by rmhuneineh on 02/05/2017.
+ */
+
+public class HabitDbHelper extends SQLiteOpenHelper {
+}
