d8ff3f8a155fe80c94156dd881b9dda6bc7a9a45
Ragheed Ma'rouf Huneineh <ragheedmhuneineh@outlook.com>
2017-05-02 11:44:55 +0200

    Define all constants in the contract

--- app/src/main/java/com/example/android/habittracker/HabitContract.java
+++ app/src/main/java/com/example/android/habittracker/HabitContract.java
@@ -1,8 +1,20 @@
 package com.example.android.habittracker;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by rmhuneineh on 02/05/2017.
  */
 
 public class HabitContract {
+
+    public static final class HabitEntry implements BaseColumns {
+        //Table Name
+        public static final String HABIT_TABLE_NAME = "habits";
+        //Table Columns
+        public static final String HABIT_ID = BaseColumns._ID;
+        public static final String HABIT_NAME = "name";
+        public static final String HABIT_DURATION = "duration";
+        public static final String HABIT_DATE = "date";
+    }
 }
