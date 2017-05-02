1fb9606e2a446322dd7ea5de77b6d9b4b89b9995
Ragheed Ma'rouf Huneineh <ragheedmhuneineh@outlook.com>
2017-05-02 12:50:29 +0200

    Apply database code

--- app/src/main/java/com/example/android/habittracker/MainActivity.java
+++ app/src/main/java/com/example/android/habittracker/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.habittracker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HabitDbHelper dbHelper = new HabitDbHelper(this);
+
+        ContentValues values = new ContentValues();
+        values.put(HabitContract.HabitEntry.HABIT_NAME, "Reading");
+        values.put(HabitContract.HabitEntry.HABIT_DURATION, 2); //Note that duration is in hrs and is considered as an integer.
+        values.put(HabitContract.HabitEntry.HABIT_DATE, "2/05/2017");
+        dbHelper.insertData(values);
+
+        /**
+         * In order to check for the insertion, use log messages!
+         */
+        Cursor cursor = dbHelper.readData(1);
+        Log.v("MainActivity", cursor.toString());
     }
 }
